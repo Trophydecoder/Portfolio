ebddc9003c4ab0d5e3b5ce4bed16366b498dd4d8
Trophydecoder <trophydecoder@gmail.com>
2024-08-10 21:49:34 +0200

    first commit

---
+++ myscriptIndex.js
@@ -0,0 +1,8 @@
+//NAvBAR slide//
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
